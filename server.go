afa8516955bf0ed8afa07f3a9ac61fe0852fae29
IrishFagan <irishfagan@live.com>
2021-02-17 13:26:39 -0800

    Implement basic functionality to recieve messages

--- server.go
+++ server.go
@@ -8,11 +8,33 @@ import (
 	"github.com/gorilla/websocket"
 )
 
-const addr = flag.String("addr", "192.168.0.28:8080", "http service address")
+var addr = flag.String("addr", "192.168.0.28:8080", "http service address")
 
-const upgrader = websocket.Upgrader{}
+var upgrader = websocket.Upgrader{}
+
+func recieveCommand(w http.ResponseWriter, r *http.Request) {
+	log.Printf("Connection")
+
+	c, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Println("Upgrade: ", err)
+		return
+	}
+	defer c.Close()
+	for {
+		mt, message, err := c.ReadMessage()
+		if err != nil {
+			log.Println("Read: ", err)
+			break
+		}
+		log.Printf("Message: %s", message)
+		log.Printf("mt: %s", mt)
+	}
+}
 
 func main() {
 	flag.Parse()
-	http.HandleFunc("/", recieve)
+	log.SetFlags(0)
+	http.HandleFunc("/", recieveCommand)
+	log.Fatal(http.ListenAndServe(*addr, nil))
 }
